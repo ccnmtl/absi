fe933e7d1359c553658030078a7fe8a61f02ffb6
Nikolas Nyby <nnyby@columbia.edu>
2026-01-22 14:01:04 -0500

    Update docker config for celery
    Allow entrypoint.sh to call celery or gunicorn, based on a variable.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,15 @@ fi
 
 python manage.py migrate --noinput --settings=$SETTINGS
 
-python manage.py collectstatic --noinput --clear --settings=$SETTINGS
+if [ "$1" = "celery" ]; then
+    echo "Starting Celery worker..."
+    celery -A absi worker --loglevel=info
+else
+    python manage.py collectstatic --noinput --clear --settings=$SETTINGS
 
-# Start the Django application
-gunicorn absi.wsgi:application \
-         --env DJANGO_SETTINGS_MODULE=$SETTINGS \
-         --bind 0.0.0.0:80 \
-         --workers 2
+    # Start the Django application
+    gunicorn absi.wsgi:application \
+             --env DJANGO_SETTINGS_MODULE=$SETTINGS \
+             --bind 0.0.0.0:80 \
+             --workers 2
+fi
