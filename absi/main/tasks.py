db95241b1b22ec86dfb51b90191cf4130b08a510
Nikolas Nyby <nnyby@columbia.edu>
2026-02-03 15:26:36 -0500

    AWS Transcribe: update language to standard arabic
    https://docs.aws.amazon.com/transcribe/latest/dg/supported-languages.html

--- absi/main/tasks.py
+++ absi/main/tasks.py
@@ -19,7 +19,7 @@ def start_transcribe_job(s3_uri: str, job_name: str) -> str:
     print('queueing job', job_name)
     transcribe.start_transcription_job(
         TranscriptionJobName=job_name,
-        LanguageCode='en-US',
+        LanguageCode='ar-SA',
         Media={
             'MediaFileUri': s3_uri,
         },
