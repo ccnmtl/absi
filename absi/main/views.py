5f71496fecc3fae25750f1e42155bd695d0900d7
Nikolas Nyby <nnyby@columbia.edu>
2026-04-29 16:41:58 -0400

    Add multiple audio sources
    Default to ogg, fall back to mp3. Increase default sample rate to 44100
    - standard CD quality.

--- absi/main/views.py
+++ absi/main/views.py
@@ -57,7 +57,8 @@ class PollyAudioView(LoginRequiredMixin, View):
             region_name=settings.AWS_REGION,
         ).client('polly')
         text = kwargs.get('text', '')
-        voice = request.GET.get('voice', '')
+        voice_param = request.GET.get('voice', '')
+        audio_format_param = request.GET.get('audio_format', '')
 
         if not text:
             return JsonResponse({'error': 'Text is required'}, status=400)
@@ -69,21 +70,31 @@ class PollyAudioView(LoginRequiredMixin, View):
             }, status=400)
 
         voice_id = 'Hala'
-        if voice == 'Zayd':
+        if voice_param == 'Zayd':
             voice_id = 'Zayd'
+
+        audio_format = 'ogg_vorbis'
+        audio_extension = 'ogg'
+        content_type = 'audio/ogg'
+        if audio_format_param == 'mp3':
+            audio_format = 'mp3'
+            audio_extension = 'mp3'
+            content_type = 'audio/mpeg'
 
         response = polly_client.synthesize_speech(
             VoiceId=voice_id,
-            OutputFormat='mp3',
+            OutputFormat=audio_format,
+            SampleRate='44100',
             Text=text,
             Engine='neural')
 
         audio_stream = response['AudioStream'].read()
         return HttpResponse(
             audio_stream,
-            content_type='audio/mpeg',
+            content_type=content_type,
             headers={
-                'Content-Disposition': "inline; filename='speech.mp3'"
+                'Content-Disposition':
+                "inline; filename=speech.{}".format(audio_extension)
             })
 
 
