a7f4f5f5266034565ab97853f096fcfe3aeae2bb
Nikolas Nyby <nnyby@columbia.edu>
2026-04-30 14:31:41 -0400

    Allow msg to be empty in websockets JS code
    If the user spoke no text, an empty message may be valid.
    
    Also, i've adjusted the DOMContentLoaded process here to make things a
    little cleaner - displayMessage() is simpler now.

--- media/js/src/websockets.js
+++ media/js/src/websockets.js
@@ -1,37 +1,37 @@
 const displayMessage = function(msg, azure) {
-    $(document).ready(function() {
-        let el = $('#transcription-result');
-        if (azure) {
-            el = $('#azure-transcription-result>pre');
-        };
+    let el = $('#transcription-result');
+    if (azure) {
+        el = $('#azure-transcription-result>pre');
+    };
 
-        if (el && msg) {
-            if (typeof msg === 'object') {
-                msg = JSON.stringify(msg, null, '  ');
-            }
-
-            $(el).text(msg);
+    if (el) {
+        if (typeof msg === 'object') {
+            msg = JSON.stringify(msg, null, '  ');
         }
-    });
+
+        $(el).text(msg);
+    }
 };
 
 const socket = new WebSocket(
     'wss://' + window.location.host + '/ws/'
 );
 
-socket.onmessage = function(e) {
-    const data = JSON.parse(e.data);
-    console.log('onmessage', data);
+document.addEventListener('DOMContentLoaded', () => {
+    socket.onmessage = function(e) {
+        const data = JSON.parse(e.data);
+        console.log('onmessage', data);
 
-    if (data && data.message) {
-        displayMessage(data.message, data.azure);
-    }
-};
+        if (data && data.message) {
+            displayMessage(data.message, data.azure);
+        }
+    };
 
-socket.onclose = function(e) {
-    const errorMessage = 'Socket closed unexpectedly';
+    socket.onclose = function(e) {
+        const errorMessage = 'Socket closed unexpectedly';
 
-    displayMessage(errorMessage);
-    displayMessage(errorMessage, true);
-    console.error(errorMessage);
-};
+        displayMessage(errorMessage);
+        displayMessage(errorMessage, true);
+        console.error(errorMessage);
+    };
+});
