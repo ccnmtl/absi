ff38132ed8b19a71c01b867674f87e6e164cb355
Nikolas Nyby <nnyby@columbia.edu>
2026-06-05 18:00:15 -0400

    Fix another score bug

--- media/js/src/websockets.js
+++ media/js/src/websockets.js
@@ -72,7 +72,7 @@ document.addEventListener('DOMContentLoaded', () => {
         if (data && data.message) {
             displayMessage(data.message, data.azure);
 
-            if (typeof score !== 'null') {
+            if (typeof score !== 'undefined') {
                 showToast(
                     data.azure ? 'Azure Speech' : 'AWS Transcribe',
                     'Your score: ' + score, 'now');
